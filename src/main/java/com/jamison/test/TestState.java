04d4a394ffd5d91a2da2812fca8b41fde6d84d1d
jamison <2810947907@qq.com>
2022-12-10 18:50:10 +0800

    Thread State

--- src/main/java/com/jamison/test/TestState.java
+++ src/main/java/com/jamison/test/TestState.java
@@ -4,6 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * @author jamison
+ */
 @Slf4j(topic = "c.TestState")
 public class TestState {
     public static void main(String[] args) throws InterruptedException {
